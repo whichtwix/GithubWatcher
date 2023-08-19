b9fbf335a0a899a79be7e35f284b92edc8768948
twix <91138333+whichtwix@users.noreply.github.com>
2023-08-19 16:37:16 +0600

    functionality to fetch multiple events

--- src/main.py
+++ src/main.py
@@ -21,11 +21,11 @@ allrepos = []
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     # register repositories like below, microsoft/dotnet is the repo taken as example here
-    #example = GithubWatcher("https://api.github.com/repos/microsoft/dotnet/events") 
+    # example = GithubWatcher("https://api.github.com/repos/microsoft/dotnet/events") 
   
 
     for i in allrepos:
-      await i.SetEtag()
+      await i.SetEtagAndId()
 
     looprepos.start()
 
@@ -49,12 +49,16 @@ class GithubWatcher:
       "authorization" : f"token {os.getenv('git_token')}", 
       "Accept" : "application/vnd.github+json"
     }
+    self.lastid = 0
     allrepos.append(self)
   
-  async def SetEtag(self):
+  async def SetEtagAndId(self):
     url = http.request('GET', url=self.url , headers=self.Headers)
     self.Headers["if-none-match"] = url.headers["ETag"]
+    data = json.loads(url.data)
+    self.lastid = data[0]['id']
     print(f'etag set for {self.url[29:].replace("/events", "")}: {url.headers["ETag"]}')
+    print(f'last event id set for {self.url[29:].replace("/events", "")}: {self.lastid}')
 
   async def checkgithub(self):
     id = 0 # edit to your channel id here, it must be a integer
@@ -62,22 +66,29 @@ class GithubWatcher:
 
     url = http.request('GET', 'https://api.github.com/rate_limit', headers=self.Headers)
     data = json.loads(url.data)
-    print(f'rate left {data["resources"]["core"]["remaining"]}')
     if data["resources"]["core"]["remaining"] == 0:
+      print('rate limited')
       return
 
     url = http.request('GET',url=self.url, headers=self.Headers)
 
-    if url.status != 304 and url.status == 200:
-      # only want the latest event from the list of events
+    if url.status == 200:
+      # only want the latest event from the list of events at a time
       data = json.loads(url.data)
-      embed = MakeEmbed(data[0])
-      if embed != None:
-        await channel.send(embed=embed)
+      for i in data:
+        if i['id'] != self.lastid:
+          embed = MakeEmbed(i)
+          if embed != None:
+            await channel.send(embed=embed)
+        elif i['id'] == self.lastid:
+          break
       self.Headers["if-none-match"] = url.headers["ETag"]
+      id = self.lastid
+      self.lastid = data[0]['id']
+      print(f'{self.url[29:].replace("/events", "")}: {id} to {self.lastid}')
       return
 
-    print(f"http code {url.status}  {self.url[29:].replace('/events', '')}")
+    print(f"http code {url.status} {self.url[29:].replace('/events', '')}")
     
 keep_alive()
 bot.run(os.getenv('TOKEN'))
